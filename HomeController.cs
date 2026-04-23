10cf02a0b23fc30841300448d0b0fc574a21365d
NavaXue <144755877+NavaXue@users.noreply.github.com>
2026-04-23 16:37:48 +0800

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,18 @@
+﻿using eAppsMobileCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace eAppsMobileCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
